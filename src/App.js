c262ebdbc0228decae76c062632490733d945b1b
Salwa99 <s.ritage99@gmail.com>
2023-01-11 13:58:54 +0100

    Add logic to the Calculator

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import MathMagicianApp from './components/Calculator';
 import './index.css';
 // eslint-disable-next-line
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return <MathMagicianApp />;
   }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import calculate from './logic/calculate';
 // eslint-disable-next-line
 export default class MathMagicianApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  btnEventHandler = (event) => {
+    const result = calculate(this.state, event.target.textContent);
+    this.setState((state0) => ({
+      ...state0,
+      ...result,
+    }));
+  }
+
   render() {
+    const { total, next } = this.state;
+
     return (
       <div className="App">
 
         <div className="calculator">
           <div className="result">
-            0
+            {next || total || 0}
           </div>
 
           <div className="wrapper">
@@ -15,34 +35,34 @@ export default class MathMagicianApp extends React.Component {
             <div className="main-cal">
 
               <div className="top-cal">
-                <button type="button">AC</button>
-                <button type="button">+/-</button>
-                <button type="button">%</button>
+                <button type="button" onClick={this.btnEventHandler}>AC</button>
+                <button type="button" onClick={this.btnEventHandler}>+/-</button>
+                <button type="button" onClick={this.btnEventHandler}>%</button>
               </div>
 
               <div className="digits-numbers">
-                <button type="button">7</button>
-                <button type="button">8</button>
-                <button type="button">9</button>
-                <button type="button">4</button>
-                <button type="button">5</button>
-                <button type="button">6</button>
-                <button type="button">1</button>
-                <button type="button">2</button>
-                <button type="button">3</button>
+                <button type="button" onClick={this.btnEventHandler}>7</button>
+                <button type="button" onClick={this.btnEventHandler}>8</button>
+                <button type="button" onClick={this.btnEventHandler}>9</button>
+                <button type="button" onClick={this.btnEventHandler}>4</button>
+                <button type="button" onClick={this.btnEventHandler}>5</button>
+                <button type="button" onClick={this.btnEventHandler}>6</button>
+                <button type="button" onClick={this.btnEventHandler}>1</button>
+                <button type="button" onClick={this.btnEventHandler}>2</button>
+                <button type="button" onClick={this.btnEventHandler}>3</button>
               </div>
               <div className="bottom-cal">
-                <button type="button">.</button>
-                <button type="button" className="btn0">0</button>
+                <button type="button" onClick={this.btnEventHandler}>.</button>
+                <button type="button" className="btn0" onClick={this.btnEventHandler}>0</button>
               </div>
             </div>
 
             <div className="operators">
-              <button type="button">÷</button>
-              <button type="button">x</button>
-              <button type="button">-</button>
-              <button type="button">+</button>
-              <button type="button">=</button>
+              <button type="button" onClick={this.btnEventHandler}>÷</button>
+              <button type="button" onClick={this.btnEventHandler}>x</button>
+              <button type="button" onClick={this.btnEventHandler}>-</button>
+              <button type="button" onClick={this.btnEventHandler}>+</button>
+              <button type="button" onClick={this.btnEventHandler}>=</button>
             </div>
           </div>
 
